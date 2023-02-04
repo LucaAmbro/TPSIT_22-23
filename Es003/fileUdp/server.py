2f803cb25a5fe70ab5782bfcb0a232476ac00cbc
Ambrosino Luca <lucambrosino04@gmail.com>
2023-02-04 14:17:12 +0100

    scambio file client-server con protocolli TCP e UDP

---
+++ Es003/fileUdp/server.py
@@ -0,0 +1,32 @@
+from packet import Packet
+from socket import socket, AF_INET, SOCK_DGRAM
+
+#MAX_SIZE = 131072
+MAX_SIZE = 8192
+def write_file(buffer):
+    with open("result.pdf", "wb") as f:
+        f.write(buffer)
+
+def run_server():
+    with socket(AF_INET, SOCK_DGRAM) as s:
+        s.bind(('0.0.0.0', 5000))
+        file = []
+        print("In ascolto")
+        while True:
+            msg = s.recvfrom(MAX_SIZE)
+            #print(msg)
+            packet = Packet.from_bytes(msg[0])
+            if packet.status == Packet.NEW_FILE:
+                file = []
+                #print("invio")
+
+            if packet.data and len(packet.data) > 0:
+                file.append(packet.data)
+                #print("boo")
+
+            if packet.status == Packet.END_FILE:
+                write_file(b''.join(file))
+                #print("fine")
+
+if __name__ == "__main__":
+    run_server()
